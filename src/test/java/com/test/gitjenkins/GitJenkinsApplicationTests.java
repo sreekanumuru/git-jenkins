83a212eddc064ba1a2e6ea5963a4bf859cb5bb48
sreekanumuru <sreekanumuru@gmail.com>
2021-09-19 20:14:33 +0530

    Second commit of for jenkins test..

--- src/test/java/com/test/gitjenkins/GitJenkinsApplicationTests.java
+++ src/test/java/com/test/gitjenkins/GitJenkinsApplicationTests.java
@@ -3,13 +3,12 @@ package com.test.gitjenkins;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class GitJenkinsApplicationTests {
 	public static Logger log = LoggerFactory.getLogger(GitJenkinsApplication.class);
